72eb124570929c6f9f761b4d89d0dfcdde3fec3e
LizPing <13377139758@163.com>
2026-07-15 17:34:28 +0800

    Add Notes on Vec07:顺序判断

---
+++ Data_Structure_MOOC_202506/Notebooks/Vector/Vector_07.cpp
@@ -0,0 +1,173 @@
+/* 07:有序向量 */
+/* 顺序判断 */
+#include <iostream>
+#include <algorithm>
+
+using Rank = int;
+constexpr int DEFAULT_CAPACITY = 3;
+
+template <typename T>
+class Vector{
+    private:
+        Rank _size;         // 规模
+        int _capacity;      // 容量
+        T* _elem;           // 数据区
+    
+    public:
+        // 类构造函数
+        Vector(int c=DEFAULT_CAPACITY);
+        Vector(const T* A, Rank low, Rank high);
+        Vector(const Vector<T> &V, Rank low, Rank high);
+        void copyFrom(const T* A, Rank lo, Rank hi);
+
+        // 类析构函数
+        ~Vector();
+
+        // 向量扩容函数
+        void expand();
+
+        // 元素访问函数
+        T& operator[](Rank r) const;
+
+        // 元素增加函数
+        Rank insert(Rank r, const T& e);
+
+        // 元素删除函数
+        int remove(Rank lo, Rank hi);
+
+        // 元素查找函数
+        Rank find(const T& e, Rank lo, Rank hi) const;
+
+        // 元素去重函数
+        int deduplicate();
+
+        // 顺序判断函数
+        int disordered() const;
+};
+
+
+int  main()
+{
+    Vector<int> vec{5};
+    vec.insert(0,4); vec.insert(1,5); vec.insert(2,5); vec.insert(2,1); vec.insert(3,3); vec.insert(3,3); vec.insert(4,2);
+    std::cout << vec.disordered() << std::endl;
+    return 0;
+}
+
+template <typename T>
+Vector<T>::Vector(int c)
+{
+    _elem = new T[_capacity = c];
+    _size = 0;
+}
+
+template <typename T>
+Vector<T>::Vector(const Vector<T> &V, Rank low, Rank high)
+{
+    copyFrom(V._elem, low, high);
+}
+
+template <typename T>
+Vector<T>::Vector(const T* A, Rank low, Rank high)
+{
+    copyFrom(A, low, high);
+}
+
+template <typename T>
+void Vector<T>::copyFrom(const T* A, Rank lo, Rank hi)
+{
+    _capacity = 2*(hi-lo);
+    _elem = new T[_capacity];
+    _size = 0;
+    while(lo < hi)
+    {
+        _elem[_size] = A[lo];
+        _size += 1;
+        lo += 1;
+    }
+}
+
+template <typename T>
+Vector<T>::~Vector()
+{
+    delete[] _elem;
+}
+
+template <typename T>
+void Vector<T>::expand()
+{
+    if (_size < _capacity) return;                      // 尚未满员时不必扩容
+    _capacity = std::max(_capacity, DEFAULT_CAPACITY);  // 不低于最小容量
+
+    T* oldElem = _elem;                 // 保存旧的数据
+    _elem = new T[_capacity <<= 1];     // 申请新的空间
+    for (int i=0; i<_size; i++)         // 新旧数据转移
+    {
+        _elem[i] = oldElem[i];
+    }
+    delete[] oldElem;
+}
+
+template <typename T>
+T& Vector<T>::operator[](Rank r) const
+{
+	return _elem[r];
+}
+
+template <typename T>
+Rank Vector<T>::insert(Rank r, const T& e)
+{
+	expand();	// 如有必要则扩容
+	for (int i = _size; i>r; i--)	// 自后向前
+	{
+		_elem[i] = _elem[i-1];		// 后继元素依次后移一个单位
+	}
+	_elem[r] = e; _size += 1;		// 置入新元素，更新容量
+	return r;
+}
+
+template <typename T>	// 删除区间[lo,hi)
+int Vector<T>::remove(Rank lo, Rank hi)
+{
+	if (lo == hi) {return 0;}
+	while (hi < _size) {_elem[lo++] = _elem[hi++];}
+	
+	_size = lo;			// 更新规模
+	return hi-lo;		// 返回被删除元素的个数
+}
+
+// 元素查找函数：
+template <typename T>	// 0 <= lo < hi <= _size
+Rank Vector<T>::find(const T& e, Rank lo, Rank hi) const	// 逆向查找：在命中多个元素时返回秩最大者
+{
+	while ((lo<hi--) && (e!=_elem[hi])){}				    // 逆向查找
+	return hi;			// 返回最终停止的索引：hi<lo意味着查找失败，否则hi即命中元素的秩。
+}
+
+// 元素去重函数
+template <typename T>
+int Vector<T>::deduplicate()
+{
+    int oldSize = _size;    // 记录原规模
+    Rank i = 1;             // 从_elem[1]开始
+    while(i < _size)        // 自前向后逐一考察各元素
+    {
+        (find(_elem[i], 0, i) < 0)  // 在前缀中寻找雷同者
+        ? i++                       // 若无雷同者，则往后继续查
+        : remove(i,i+1);            // 若有雷同者，则删除雷同者
+    }
+    return oldSize - _size;         // 返回向量被删除的个数
+}
+
+
+// 顺序判断函数
+template <typename T>
+int Vector<T>::disordered() const
+{
+    int n = 0;
+    for (int i=1; i<_size; i++)
+    {
+        n += int(T[i-1] > T[i]);
+    }
+    return n;
+}
